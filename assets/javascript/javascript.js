a90d0e77cdd1f50349f171273cbc627bfc3ccb4b
Genjosh <41222199+Genjosh@users.noreply.github.com>
2018-08-17 13:18:34 -0600

    javascript update TTS

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -78,3 +78,23 @@ $("#add-information-btn").on("click", function (event) {
 
 $("form")[0].reset();
 
+
+// TTS javascript
+var audioClip;
+var alarm;
+
+var queryUrl ="http://api.voicerss.org/?key=0a7914a1376346ce8cee9c5045328467&hl=en-us&b64=true&src=it is 1 o'clock";
+  $.ajax({
+    url:queryUrl,
+    method:"get",
+    
+  }).then(function(response) {
+      audioClip = document.createElement("audio");
+      audioClip.setAttribute("src", response);
+
+  });
+
+  $("#clickMe").on("click",function(){
+    audioClip.play();
+  });
+
