1b17d82a8eb5c5ec6da22d6241c0b5a9c17fbae8
Genjosh <41222199+Genjosh@users.noreply.github.com>
2018-08-17 13:03:09 -0600

    yes

--- assets/app.js
+++ assets/app.js
@@ -1,19 +1,27 @@
 
-var settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": "http://unirest.io/HttpResponse%3CInputStream%3E%20response%20=%20Unirest.get%28%22https://montanaflynn-text-to-speech.p.mashape.com/speak",
-    "method": "GET",
-    "headers": {
-      "Cache-Control": "no-cache",
-      "Postman-Token": "dbfec823-7a47-4723-86b7-0b2d10c88aa6"
-    }
-  };
+var audioClip;
+var queryUrl ="http://api.voicerss.org/?key=0a7914a1376346ce8cee9c5045328467&hl=en-us&b64=true&src=You now have 15 minutes to get on the bus!";
+  $.ajax({
+    url:queryUrl,
+    method:"get",
+    
+  }).then(function(response) {
+      audioClip = document.createElement("audio");
+      audioClip.setAttribute("src", response);
 
+  });
+
+  $("#clickMe").on("click",function(){
+    audioClip.play();
+  });
+
+  setInterval(function() {
+    audioClip.play();
+  } ,1000);
+  
 
   
-  $.ajax(settings).done(function (response) {
-    console.log(response);
-
+   
+  // $.ajax(settings).done(function (response) {
     
-  });
+
