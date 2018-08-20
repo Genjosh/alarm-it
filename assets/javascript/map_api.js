d2df75cb27d6fa33feedfc56209faddc12e2bd7a
Genjosh <41222199+Genjosh@users.noreply.github.com>
2018-08-20 10:42:43 -0600

    test

--- assets/javascript/map_api.js
+++ assets/javascript/map_api.js
@@ -78,26 +78,3 @@ $("#add-information-btn").on("click", function (event) {
 
 $("form")[0].reset();
 
-
-// TTS javascript
-var audioClip;
-var alarm;
-
-var queryUrl ="http://api.voicerss.org/?key=0a7914a1376346ce8cee9c5045328467&hl=en-us&b64=true&src=hello world";
-  $.ajax({
-    url:queryUrl,
-    method:"get",
-    
-  }).then(function(response) {
-      audioClip = document.createElement("audio");
-      audioClip.setAttribute("src", response);
-
-
-  });
-
-  $("#add-info").on("click",function(){
-    
-    console.log(then);
-
-  });
-
